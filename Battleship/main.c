4d18f9a6cd8300b9efa69ee563efb074aeacacb7
Daniel Regnard <daniel.regnard@efrei.net>
2017-12-27 14:36:44 +0100

    Initial Commit

---
+++ Battleship/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Battleship
+//
+//  Created by Daniel Regnard on 27/12/2017.
+//  Copyright © 2017 Daniel Regnard. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
